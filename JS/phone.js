156127a6d1a0e805c88c7ad4b2ed95e436f9aaaa
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-10 02:02:24 +0600

    Display All Phones Data

--- JS/phone.js
+++ JS/phone.js
@@ -1,12 +1,30 @@
-const loadPhones = async () =>{
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+const loadPhones = async (serachText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${serachText}`
     const res = await fetch(url)
     const data = await res.json()
     displayPhones(data.data)
 }
 
-const displayPhones = phones =>{
-    console.log(phones)
-}
+const displayPhones = phones => {
+    const phonesContainer = document.getElementById('phone-container');
+    phonesContainer.innerText = '';
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('single-phone')
+        phoneDiv.innerHTML = `
+        <img class="w-2/4" src="${phone.image}">
+        <p class="font-bold  text-2xl text-gray-600">${phone.phone_name}</p>
+        <p class="font-semibold text-center text-sm">${phone.slug}</p>
+     `
+        phonesContainer.appendChild(phoneDiv);
+    })
+} 
+
+document.getElementById('btn-search').addEventListener('click', function(){
+     const searchField = document.getElementById('search-field');
+     const serachText = searchField.value;
+     loadPhones(serachText)
+
+}) 
 
 loadPhones()
