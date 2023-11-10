4d1478fcdb4a80f67666c77a07d710118dfd820a
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-10 20:53:12 +0600

    Seacrh by Enter Key and Show Phone Details ID

--- JS/phone.js
+++ JS/phone.js
@@ -5,6 +5,8 @@ const loadPhones = async (serachText, limitdata) => {
     displayPhones(data.data, limitdata)
 }
 
+
+
 const displayPhones = (phones, limitdata) => {
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.innerText = '';
@@ -17,6 +19,8 @@ const displayPhones = (phones, limitdata) => {
         showAll.classList.add('show-all')
     }
 
+
+
     //display no phone found
     const noPhone = document.getElementById('no-found-message');
     if (phones.length === 0) {
@@ -26,6 +30,9 @@ const displayPhones = (phones, limitdata) => {
         noPhone.classList.add('display-none')
     }
     toggleLoader(false);
+
+
+
     // display all phone
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
@@ -34,12 +41,15 @@ const displayPhones = (phones, limitdata) => {
         <img class="w-2/4" src="${phone.image}">
         <p class="font-bold  text-2xl text-gray-600">${phone.phone_name}</p>
         <p class="font-semibold text-center text-sm">${phone.slug}</p>
-     `
+        <button onclick="loadPhoneDetails('${phone.slug}')" class="bg-blue-600 text-white rounded py-2 px-5 mt-5">Details</button>
+     `  
         phonesContainer.appendChild(phoneDiv);
         // stop loader
         toggleLoader(false);
     });
 }
+
+
 
 const processData = (limitdata) => {
     toggleLoader(true);
@@ -48,10 +58,21 @@ const processData = (limitdata) => {
     loadPhones(serachText, limitdata)
 }
 
+
+
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        processData(5);
+    }
+})
+
+
+
 document.getElementById('btn-search').addEventListener('click', function () {
     processData(5);
+})
 
-}) 
+
 
 const toggleLoader = isLoading => {
     const loaderSpiner = document.getElementById('loader');
@@ -63,8 +84,19 @@ const toggleLoader = isLoading => {
     }
 }
 
-document.getElementById('btn-show-all').addEventListener('click', function(){
+
+
+document.getElementById('btn-show-all').addEventListener('click', function () {
     processData();
 })
+
+
+
+const loadPhoneDetails = async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data)
+}
 
 // loadPhones()
